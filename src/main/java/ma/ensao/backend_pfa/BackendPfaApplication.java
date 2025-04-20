4892090b25c76877dada171f2c66ef9db483d546
Marouan Ouadghiri <Marouan@Ouadghiri.com>
2025-04-21 00:51:35 +0100

    register  ok hard core ok projcts ok

--- src/main/java/ma/ensao/backend_pfa/BackendPfaApplication.java
+++ src/main/java/ma/ensao/backend_pfa/BackendPfaApplication.java
@@ -22,7 +22,7 @@ public class BackendPfaApplication {
         SpringApplication.run(BackendPfaApplication.class, args);
     }
 
-    @Bean
+   /* @Bean
     CommandLineRunner start(UserService userService, RoleRepository roleRepository, PasswordEncoder passwordEncoder,ProjectRepository projectRepository) {
         return args -> {
             // Créer des rôles
@@ -77,6 +77,8 @@ public class BackendPfaApplication {
 
         };
     }
+
+    */
 
 
 
